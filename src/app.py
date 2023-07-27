73313ef55b57eb5d8dec1fb8ffe585e6d44bc27e
Jona Rams <jona.rams@logobject.ch>
2023-07-27 14:34:26 +0200

    query select and find function

--- src/app.py
+++ src/app.py
@@ -1,9 +1,8 @@
 from flask import Flask
-from common.database import initialize
+from common.database import Database
 
 app = Flask(__name__)
-initialize()
-
+Database.initialize("clicker")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- src/common/database.py
+++ src/common/database.py
@@ -1,34 +1,66 @@
 import psycopg2
 
-def initialize():
-    dbname = "clicker"
-    user = "postgres"
-    password = "!C0mplex"
-    host = "localhost"
-    port = "5432"
+class Database(object):
+    URI = "localhost"
+    connection = None
 
-    try:
-        # Connect to the existing database
-        connection = psycopg2.connect(
-            dbname=dbname,
-            user=user,
-            password=password,
-            host=host,
-            port=port
-        )
+    @staticmethod
+    def initialize(dbname):
+        user = "postgres"
+        password = "!C0mplex"
+        host = Database.URI
+        port = "5432"
 
-        print("Connected to the PostgreSQL database successfully.")
+        try:
+            Database.connection = psycopg2.connect(
+                dbname=dbname,
+                user=user,
+                password=password,
+                host=host,
+                port=port
+            )
 
-        # You can perform database operations here, for example:
-        # cursor = connection.cursor()
-        # cursor.execute("SELECT * FROM your_table_name;")
-        # rows = cursor.fetchall()
-        # print(rows)
+            print("Connected to the PostgreSQL database successfully.")
 
-    except psycopg2.Error as e:
-        print(f"Error connecting to the database: {e}")
+        except psycopg2.Error as e:
+            print(f"Error connecting to the database: {e}")
+            return None
 
-    finally:
-        # Close the connection
-        if connection:
-            connection.close()
+    @staticmethod
+    def find_one(connection, table_name, query):
+        try:
+            cursor = connection.cursor()
+            sql_query = f"SELECT * FROM {table_name} WHERE {query};"
+            cursor.execute(sql_query)
+            rows = cursor.fetchall()
+
+            # Print or process the results
+            for row in rows:
+                print(row)
+
+        except psycopg2.Error as e:
+            print(f"Error executing query: {e}")
+
+        finally:
+            if cursor:
+                cursor.close()
+
+    @staticmethod
+    def insert(connection, table_name, data):
+        try:
+            cursor = connection.cursor()
+            columns = ', '.join(data.keys())
+            values = ', '.join([f"'{value}'" for value in data.values()])
+            query = f"INSERT INTO {table_name} ({columns}) VALUES ({values});"
+            cursor.execute(query)
+            connection.commit()
+
+            print("Data inserted successfully.")
+
+        except psycopg2.Error as e:
+            print(f"Error inserting data: {e}")
+
+        finally:
+            if cursor:
+                cursor.close()
+
