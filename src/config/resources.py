fce3da094e79230308bb18cfa2a017954ac1a238
Jona Rams <jona.rams@logobject.ch>
2023-08-09 15:39:06 +0200

    jwt

--- src/config/resources.py
+++ src/config/resources.py
@@ -1,3 +1,4 @@
+from flask_jwt_extended import JWTManager
 from flask_restful import Api, Resource
 from service.geoarea_service import GeoAreas
 from service.pollution_service import Pollution
@@ -5,6 +6,7 @@ from service.auth_service import Login
 
 def set_resources(_app):
     api = Api(_app)
+    jwt = JWTManager(_app)
 
     api.add_resource(GeoAreas, '/geoarea')
     api.add_resource(Pollution, '/pollution/byGeoarea_fk/<geoarea_fk>')
