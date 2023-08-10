37b9dfaa64201c8ab1ed963dd8ea4ed7a14d4ffe
Jona Rams <jona.rams@logobject.ch>
2023-08-10 20:55:06 +0200

    inital commit

--- src/config/resources.py
+++ src/config/resources.py
@@ -6,6 +6,7 @@ from service.auth_service import Login
 
 def set_resources(_app):
     api = Api(_app)
+    jwt = JWTManager(_app)
 
     api.add_resource(GeoAreas, '/geoarea')
     api.add_resource(Pollutions, '/pollution/byGeoarea_fk/<geoarea_fk>')
