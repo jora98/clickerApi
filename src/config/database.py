d145cb9004249b240f8122502f4e78fc415f1309
Jona Rams <jona.rams@logobject.ch>
2023-09-04 11:13:59 +0200

    yaml_2

--- src/config/database.py
+++ src/config/database.py
@@ -1,4 +1,5 @@
 from flask import Config
+import os
 
 class MainConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:!C0mplex@localhost/clicker'
@@ -8,7 +9,7 @@ class MainConfig(Config):
 class TestConfig(Config):
     TESTING = True
     JWT_SECRET_KEY = '123'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:!C0mplex@localhost/test'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 def initialize_db(_app, _db, config):
     _app.config.from_object(config)
